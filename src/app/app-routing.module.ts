8c57c63fe4b53ca52805613c3cdb4fb662640124
Swarup Mishra <swarupmishra1999@gmail.com>
2021-10-17 22:02:33 +0530

    changes v1.0

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddUrlComponent } from './add-url/add-url.component';
+import { ListUrlComponent } from './list-url/list-url.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+   {
+    path:'',redirectTo:'listURL',pathMatch:'full'
+  },
+  {
+    path:'addURL',component: AddUrlComponent
+  },
+  {
+    path:'listURL',component:ListUrlComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
